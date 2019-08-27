5b5fbb95a615b4e2d92f1b0a19c3ba225629dddf
Daniel Vadil <dvadz@yahoo.com>
2019-08-27 01:50:22 -0400

    created Home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 class App extends React.Component {
+  state = {
+    name: "Daniel Vadil",
+    description: "I am a Full Stack Developer"
+  };
+
   render() {
     return (
       <div>
         <Navbar></Navbar>
+        <Home name={this.state.name} description={this.state.description}></Home>
       </div>
     );
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,13 @@
 import React from "react";
+import "./Home.css";
+
+const Home = props => {
+  return (
+    <div className='home text-center border'>
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+    </div>
+  );
+};
+
+export default Home;
