1f07a7bbbb2a0e1ed48ad2186d48ad872a78e221
Daniel Vadil <dvadz@yahoo.com>
2019-08-30 19:58:05 -0400

    added links to social/mail icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,15 @@ const Home = props => {
         <p>{props.description}</p>
       </div>
       <div className='social'>
-        <i className='fab fa-linkedin-in'></i>
-        <i className='fab fa-github-square'></i>
+        <a href='http://linkedin.com/in/danielvadil' target='_blank'>
+          <i className='fab fa-linkedin'></i>
+        </a>
+        <a href='https://github.com/dvadz' target='_blank'>
+          <i className='fab fa-github-square'></i>
+        </a>
+        <a href='mailto:dvadz@yahoo.com'>
+          <i className='fas fa-envelope-square'></i>
+        </a>
       </div>
       <i className='fas fa-chevron-circle-down'></i>
     </div>
