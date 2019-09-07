ae957e497612141010e0226b751b54774a50bf0e
Daniel Vadil <dvadz@yahoo.com>
2019-09-06 23:25:31 -0400

    added card that will show individual projects

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./Home";
 import Navbar from "./Navbar";
 import Portfolio from "./Portfolio";
+import projects from "./projects";
 
 class App extends React.Component {
   state = {
     name: "Daniel Vadil",
     description:
-      "Full Stack Web Developer with years of experience in Software Testing. Knowledgeable in the waterfall and agile software development life cycle methodologies. Focused on the user experience."
+      "Full Stack Web Developer with years of experience in Software Testing. Knowledgeable in the waterfall and agile software development life cycle methodologies. Focused on the user experience.",
+    projects: projects
   };
 
   render() {
@@ -15,7 +17,7 @@ class App extends React.Component {
       <div className='container'>
         <Navbar></Navbar>
         <Home name={this.state.name} description={this.state.description}></Home>
-        <Portfolio></Portfolio>
+        <Portfolio projects={this.state.projects}></Portfolio>
       </div>
     );
   }
