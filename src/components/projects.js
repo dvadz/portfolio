54021ba4f214fb237f17b8f842d26904e372058f
Daniel Vadil <dvadz@yahoo.com>
2019-09-06 23:24:31 -0400

    added file that will hold array of projects to be shown

---
+++ src/components/projects.js
@@ -0,0 +1,12 @@
+const projects = [
+  {
+    title: "project1",
+    description: "project1 description"
+  },
+  {
+    title: "project2",
+    description: "project2 description"
+  }
+];
+
+export default projects;
