bf14904791ce1a6026732dc523fa026934749128
Daniel Vadil <dvadz@yahoo.com>
2019-09-11 22:19:55 -0400

    removed Portfolio fixed height, added two more placeholder cards

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -25,6 +25,44 @@ const Portfolio = () => {
             </div>
           </div>
         </div>
+
+        <div className='card mb-3'>
+          <div className='row no-gutters'>
+            <div className='col-md-4'>
+              <img src='https://via.placeholder.com/150' className='card-img' alt='project' />
+            </div>
+            <div className='col-md-8'>
+              <div className='card-body'>
+                <h5 className='card-title'>Card title</h5>
+                <p className='card-text'>
+                  Noob-board is an application where users can ask questions that are related to
+                  programming. In addition, users can comment on previos posts with answers. This
+                  app aims to provide its users with answers, knowledge and more ideas related to
+                  coding.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='card mb-3'>
+          <div className='row no-gutters'>
+            <div className='col-md-4'>
+              <img src='https://via.placeholder.com/150' className='card-img' alt='project' />
+            </div>
+            <div className='col-md-8'>
+              <div className='card-body'>
+                <h5 className='card-title'>Card title</h5>
+                <p className='card-text'>
+                  Noob-board is an application where users can ask questions that are related to
+                  programming. In addition, users can comment on previos posts with answers. This
+                  app aims to provide its users with answers, knowledge and more ideas related to
+                  coding.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
