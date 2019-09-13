51236216c90c38e6fe7f31790e0ac77493c9dfd3
Daniel Vadil <dvadz@yahoo.com>
2019-09-13 18:18:51 -0400

    trying out new layout, cleanup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,8 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div id='contact' className='contact'>
-      Contact
+      <h2 className='p-4 text-center'>Contact</h2>
+      <div className='email'></div>
     </div>
   );
 };
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./Portfolio.css";
+import noob from "../images/noob.png";
 
 const Portfolio = () => {
   return (
     <div id='portfolio' className='portfolio'>
       <h2 className='p-4 text-center'>Portfolio</h2>
 
-      <div>
-        <div className='card mb-3'>
+      <div className='card'>
+        <div className='row no-gutters'>
+          <div className='col-md-4'>
+            <img src={noob} className='card-img' alt='project' />
+          </div>
+          <div className='col-md-8'>
+            <div className='card-body'>
+              <h5 className='card-title'>Card title</h5>
+              <p className='card-text'>
+                Noob-board is an application where users can ask questions that are related to
+                programming. In addition, users can comment on previos posts with answers. This app
+                aims to provide its users with answers, knowledge and more ideas related to coding.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className='card'>
           <div className='row no-gutters'>
             <div className='col-md-4'>
               <img src='https://via.placeholder.com/150' className='card-img' alt='project' />
@@ -26,26 +43,7 @@ const Portfolio = () => {
           </div>
         </div>
 
-        <div className='card mb-3'>
-          <div className='row no-gutters'>
-            <div className='col-md-4'>
-              <img src='https://via.placeholder.com/150' className='card-img' alt='project' />
-            </div>
-            <div className='col-md-8'>
-              <div className='card-body'>
-                <h5 className='card-title'>Card title</h5>
-                <p className='card-text'>
-                  Noob-board is an application where users can ask questions that are related to
-                  programming. In addition, users can comment on previos posts with answers. This
-                  app aims to provide its users with answers, knowledge and more ideas related to
-                  coding.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='card mb-3'>
+        <div className='card'>
           <div className='row no-gutters'>
             <div className='col-md-4'>
               <img src='https://via.placeholder.com/150' className='card-img' alt='project' />
