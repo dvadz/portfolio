a56e852f650c6b582e3a53c94917f4d7d392fab9
Daniel Vadil <dvadz@yahoo.com>
2019-10-17 23:08:09 -0400

    some style tweaking

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,12 +9,12 @@ const Portfolio = () => {
 
       <div className='card'>
         <div className='row no-gutters'>
-          <div className='col-md-4'>
+          <div className='col-sm-4'>
             <img src={noob} className='card-img' alt='project' />
           </div>
-          <div className='col-md-8'>
+          <div className='col-sm-8'>
             <div className='card-body'>
-              <h5 className='card-title'>Card title</h5>
+              <h5 className='card-title'>Noob Board</h5>
               <p className='card-text'>
                 Noob-board is an application where users can ask questions that are related to
                 programming. In addition, users can comment on previos posts with answers. This app
@@ -26,29 +26,10 @@ const Portfolio = () => {
 
         <div className='card'>
           <div className='row no-gutters'>
-            <div className='col-md-4'>
+            <div className='col-sm-4'>
               <img src='https://via.placeholder.com/150' className='card-img' alt='project' />
             </div>
-            <div className='col-md-8'>
-              <div className='card-body'>
-                <h5 className='card-title'>Card title</h5>
-                <p className='card-text'>
-                  Noob-board is an application where users can ask questions that are related to
-                  programming. In addition, users can comment on previos posts with answers. This
-                  app aims to provide its users with answers, knowledge and more ideas related to
-                  coding.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className='card'>
-          <div className='row no-gutters'>
-            <div className='col-md-4'>
-              <img src='https://via.placeholder.com/150' className='card-img' alt='project' />
-            </div>
-            <div className='col-md-8'>
+            <div className='col-sm-8'>
               <div className='card-body'>
                 <h5 className='card-title'>Card title</h5>
                 <p className='card-text'>
