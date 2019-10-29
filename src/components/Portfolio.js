bc640e3c3203b1c8029cef523aad5cadf40ca5d8
Daniel Vadil <dvadz@yahoo.com>
2019-10-29 12:45:52 -0400

    rearranged the portfolio cards to line up horizontally

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,72 +6,76 @@ import rpg from "../images/rpg.png";
 
 const Portfolio = () => {
   return (
-    <div id='portfolio' className='portfolio'>
-      <h2 className='p-4 text-center'>Portfolio</h2>
+    <div id='portfolio' className='portfolio container'>
+      <div className='row'>
+        <h2 className='py-4 mx-auto'>Portfolio</h2>
+      </div>
 
-      {/* Project 1 */}
-      <div className='card'>
-        <div className='row no-gutters'>
-          <div className='col-sm-4'>
-            <img src={noob} className='card-img' alt='project' />
-          </div>
-          <div className='col-sm-8'>
-            <div className='card-body'>
-              <h5 className='card-title'>Noob Board</h5>
-              <p className='card-text'>
-                Noob-board is a community board for posting your questions posting and commenting on
-                others' questions. Also share your ideas and new discoveries.
-              </p>
-              <a
-                className='btn btn-primary'
-                href='https://noob-board.herokuapp.com/'
-                target='_blank'>
-                Open
-              </a>
+      <div className='row'>
+        {/* Project 1 */}
+        <div className='card col-sm-6 col-md-4'>
+          <div className='row no-gutters'>
+            <div>
+              <img src={noob} className='card-img' alt='project' />
+            </div>
+            <div>
+              <div className='card-body'>
+                <h5 className='card-title'>Noob Board</h5>
+                <p className='card-text'>
+                  Noob-board is a community board for posting your questions posting and commenting
+                  on others' questions. Also share your ideas and new discoveries.
+                </p>
+                <a
+                  className='btn btn-primary'
+                  href='https://noob-board.herokuapp.com/'
+                  target='_blank'>
+                  Open
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Project 2 */}
-      <div className='card'>
-        <div className='row no-gutters'>
-          <div className='col-sm-4'>
-            <img src={rapidfire} className='card-img' alt='project' />
-          </div>
-          <div className='col-sm-8'>
-            <div className='card-body'>
-              <h5 className='card-title'>Rapidfire</h5>
-              <p className='card-text'>
-                Test your basic Javascript knowledge with this quiz on a short timer.
-              </p>
-              <a
-                className='btn btn-primary'
-                href='https://dvadz.github.io/TriviaGame/'
-                target='_blank'>
-                Open
-              </a>
+        {/* Project 2 */}
+        <div className='card col-sm-6 col-md-4'>
+          <div className='row no-gutters'>
+            <div>
+              <img src={rapidfire} className='card-img' alt='project' />
+            </div>
+            <div>
+              <div className='card-body'>
+                <h5 className='card-title'>Rapidfire</h5>
+                <p className='card-text'>
+                  Test your basic Javascript knowledge with this quiz on a short timer.
+                </p>
+                <a
+                  className='btn btn-primary'
+                  href='https://dvadz.github.io/TriviaGame/'
+                  target='_blank'>
+                  Open
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* Project 3 */}
-      <div className='card'>
-        <div className='row no-gutters'>
-          <div className='col-sm-4'>
-            <img src={rpg} className='card-img' alt='project' />
-          </div>
-          <div className='col-sm-8'>
-            <div className='card-body'>
-              <h5 className='card-title'>RPG Game</h5>
-              <p className='card-text'>Pick your favorite player and defeat the rest of them.</p>
-              <a
-                className='btn btn-primary'
-                href=' https://dvadz.github.io/unit-4-game/'
-                target='_blank'>
-                Open
-              </a>
+        {/* Project 3 */}
+        <div className='card col-sm-6 col-md-4'>
+          <div className='row no-gutters'>
+            <div>
+              <img src={rpg} className='card-img' alt='project' />
+            </div>
+            <div>
+              <div className='card-body'>
+                <h5 className='card-title'>RPG Game</h5>
+                <p className='card-text'>Pick your favorite player and defeat the rest of them.</p>
+                <a
+                  className='btn btn-primary'
+                  href=' https://dvadz.github.io/unit-4-game/'
+                  target='_blank'>
+                  Open
+                </a>
+              </div>
             </div>
           </div>
         </div>
