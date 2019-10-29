0f5d8de8de8c2aabc4e1b2dffe989d1313d51836
Daniel Vadil <dvadz@yahoo.com>
2019-10-29 14:10:01 -0400

    - moved email address and phone to the bottom, updated their icons - added name input to the form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,33 +5,38 @@ const Contact = () => {
   return (
     <div id='contact' className='contact container'>
       <h2 className='p-4 text-center'>Contact Me</h2>
-      <p>
-        <span>Email: </span> <span>dtvadil@gmail.com</span>
-      </p>
-      <p className='my-4'>
-        <span>Phone: </span> <span>(647) 641-3234</span>
-      </p>
 
       <div className='email'>
         <form>
+          {/* Name */}
           <div class='form-group'>
-            <label for='exampleFormControlInput1'>Your Email address</label>
-            <input
-              type='email'
-              class='form-control'
-              id='exampleFormControlInput1'
-              placeholder='name@example.com'
-            />
+            {/* <label for='name'></label> */}
+            <input type='email' class='form-control' id='name' placeholder='Your name' />
           </div>
+          {/* Email Address */}
           <div class='form-group'>
-            <label for='exampleFormControlTextarea1'>Message</label>
-            <textarea class='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
+            {/* <label for='email'></label> */}
+            <input type='email' class='form-control' id='email' placeholder='Your email address' />
+          </div>
+          {/* Message */}
+          <div class='form-group'>
+            <label for='message'>Message</label>
+            <textarea class='form-control' id='message' rows='5'></textarea>
           </div>
           <button className='btn btn-primary' type='submit'>
             Send Email
           </button>
         </form>
       </div>
+
+      <p className='mx-2 mt-4 '>
+        <a href='mailto:dtvadil@gmail.com'>
+          <i className='fas fa-envelope mr-3'></i> dtvadil@gmail.com
+        </a>
+      </p>
+      <p className='mx-2 mt-4'>
+        <i class='fas fa-phone mr-3'></i> (647) 641-3234
+      </p>
     </div>
   );
 };
