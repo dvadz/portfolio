5ae6721dde0aef139e8d2b949db59610727664bf
Daniel Vadil <dvadz@yahoo.com>
2019-10-29 13:12:14 -0400

    - added paragraph containing phone and email address - used bootstrap to style the email form - added a Send button

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,14 +3,33 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div id='contact' className='contact'>
-      <h2 className='p-4 text-center'>Contact</h2>
+    <div id='contact' className='contact container'>
+      <h2 className='p-4 text-center'>Contact Me</h2>
+      <p>
+        <span>Email: </span> <span>dtvadil@gmail.com</span>
+      </p>
+      <p className='my-4'>
+        <span>Phone: </span> <span>(647) 641-3234</span>
+      </p>
 
       <div className='email'>
         <form>
-          <input placeholder='Your Name' required></input>
-          <input placeholder='Your Email Address' required></input>
-          <textarea required placeholder='Message'></textarea>
+          <div class='form-group'>
+            <label for='exampleFormControlInput1'>Your Email address</label>
+            <input
+              type='email'
+              class='form-control'
+              id='exampleFormControlInput1'
+              placeholder='name@example.com'
+            />
+          </div>
+          <div class='form-group'>
+            <label for='exampleFormControlTextarea1'>Message</label>
+            <textarea class='form-control' id='exampleFormControlTextarea1' rows='3'></textarea>
+          </div>
+          <button className='btn btn-primary' type='submit'>
+            Send Email
+          </button>
         </form>
       </div>
     </div>
