9d90719e5a663236ec5a9f2f4b15b4ce1dcdfa43
Daniel Vadil <dvadz@yahoo.com>
2019-10-29 12:42:57 -0400

    - moved all texts and links under one div - centered the div with absolute position and transform: translate - used linear gradient to soften the background picture in lieu of opacity - renamed classes, set width, padding, margin, letter-spacing

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,25 +7,29 @@ const Home = props => {
   return (
     <div id='home' className='home'>
       <Navbar></Navbar>
-      <div>
-        <h1 className='daniel sm-'>{props.name}</h1>
-        <p className='about-daniel'>{props.description}</p>
-      </div>
-      <div className='social'>
-        <a href='http://linkedin.com/in/danielvadil' target='_blank' rel='noopener noreferrer'>
-          <i className='fab fa-linkedin'></i>
-        </a>
-        <a href='https://github.com/dvadz' target='_blank' rel='noopener noreferrer'>
-          <i className='fab fa-github-square'></i>
-        </a>
-        <a href='mailto:dvadz@yahoo.com' rel='noopener noreferrer'>
-          <i className='fas fa-envelope-square'></i>
-        </a>
-      </div>
-      <div className='arrow'>
-        <a href='#portfolio'>
-          <i className='fas fa-chevron-circle-down'></i>
-        </a>
+      <div className='header'>
+        <div className='text-box'>
+          <h1 className='name'>{props.name}</h1>
+          <p className='about'>{props.description}</p>
+        </div>
+        {/* Social media and mailto links */}
+        <div className='social'>
+          <a href='http://linkedin.com/in/danielvadil' target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-linkedin'></i>
+          </a>
+          <a href='https://github.com/dvadz' target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-github-square'></i>
+          </a>
+          <a href='mailto:dvadz@yahoo.com' rel='noopener noreferrer'>
+            <i className='fas fa-envelope-square'></i>
+          </a>
+        </div>
+        {/* Arrow down */}
+        <div className='arrow'>
+          <a href='#portfolio'>
+            <i className='fas fa-chevron-circle-down'></i>
+          </a>
+        </div>
       </div>
     </div>
   );
