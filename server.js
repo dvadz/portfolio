b62cee2b5322f6dc874b268199675fe48c85aa6b
Daniel Vadil <dvadz@yahoo.com>
2019-12-05 21:32:25 -0500

    Fixed missing switch between production/test, missing get '*'

--- server.js
+++ server.js
@@ -10,13 +10,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} else {
+  app.use(express.static("public"));
+}
+
 app.post("/post", (req, res) => {
   console.log("Requested to send an email");
   sendEmail(req, res);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, function() {
